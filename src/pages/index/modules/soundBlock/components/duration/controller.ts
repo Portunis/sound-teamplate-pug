db43a6dc6b87ec94b8111d0374eb39c1372fc6ef
Pavel Petrov <portunisone@mail.ru>
2023-12-01 16:50:31 +0700

    feature/task-7 fix pixel perfect

--- src/pages/index/modules/soundBlock/components/duration/controller.ts
+++ src/pages/index/modules/soundBlock/components/duration/controller.ts
@@ -3,12 +3,49 @@ import 'nouislider/dist/nouislider.css'
 const useDuration = () => {
   const slider: Element | null = document.getElementById('j-duration')
   noUiSlider.create(slider as never, {
-    start: [0, 12],
+    start: [2, 100],
     connect: true,
     range: {
       min: 0,
-      max: 12
+      max: 100
     }
+  })
+  const updateSliderValue = (slider: Element | null, handle: number | null | undefined) => {
+    let children, i, results, val, values
+    if (handle == null) {
+      handle = 0
+    }
+    // eslint-disable-next-line prefer-const,@typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    // eslint-disable-next-line prefer-const
+    children = slider.getElementsByClassName('noUi-handle')
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    // eslint-disable-next-line prefer-const
+    values = slider.noUiSlider.get()
+    i = 0
+    // eslint-disable-next-line prefer-const
+    results = []
+    while (i < children.length) {
+      if (children.length === 1) {
+        val = parseInt(values)
+      } else {
+        val = parseInt(values[i])
+      }
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      children[i].dataset.value = val
+      results.push(i++)
+    }
+    return results
+  }
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  slider.noUiSlider.on('update', function () {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    return updateSliderValue(slider)
   })
 }
 export default useDuration
