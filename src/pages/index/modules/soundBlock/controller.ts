2b6579bbff0f72022217d27396ad069d781b8764
Pavel Petrov <portunisone@mail.ru>
2023-12-05 15:20:22 +0700

    feature/task-7 fix js style

--- src/pages/index/modules/soundBlock/controller.ts
+++ src/pages/index/modules/soundBlock/controller.ts
@@ -4,7 +4,7 @@ import Swiper from 'swiper'
 import 'swiper/css'
 export class SoundController {
     private readonly buttons: NodeListOf<Element>;
-    private readonly audioFile: NodeListOf<HTMLAudioElement>;
+    private readonly audioFile: HTMLAudioElement;
     private readonly playButton: NodeListOf<Element>;
     private readonly pauseButton: NodeListOf<Element>;
     private currentTimeDisplay: NodeListOf<Element>;
@@ -13,7 +13,7 @@ export class SoundController {
 
     constructor (private container: HTMLSelectElement) {
       this.buttons = this.container.querySelectorAll('.j-control-button')
-      this.audioFile = document.querySelectorAll('.sound__audio')
+      this.audioFile = document.querySelector('.sound__audio') as HTMLMediaElement
       this.playButton = this.container.querySelectorAll('.j-sound-play')
       this.pauseButton = this.container.querySelectorAll('.j-sound-pause')
       this.currentTimeDisplay = this.container.querySelectorAll('.sound__time')
@@ -43,15 +43,13 @@ export class SoundController {
     }
 
     initAudio () {
-      this.audioFile?.forEach((audio) => {
-        audio.addEventListener(
-          'loadeddata',
-          () => {
-            this.getTimeAudio(audio)
-          },
-          false
-        )
-      })
+      this.audioFile?.addEventListener(
+        'loadeddata',
+        () => {
+          this.getTimeAudio(this.audioFile)
+        },
+        false
+      )
     }
 
     initButton () {
@@ -67,53 +65,54 @@ export class SoundController {
     }
 
     checkEndAudio () {
-      this.audioFile?.forEach((audio) => {
-        audio.addEventListener('ended', () => {
-          this.playButton.forEach((item) => {
-            item.classList.remove('pause-active')
-          })
+      this.audioFile.addEventListener('ended', () => {
+        this.playButton.forEach((item) => {
+          item.classList.remove('pause-active')
         })
       })
     }
 
     togglePlayAudio () {
       if (this.audioFile) {
-        this.audioFile.forEach((audio) => {
-          this.playButton.forEach((item) => {
-            item.addEventListener('click', () => {
-              if (item.classList.contains('pause-active')) {
-                item.classList.remove('pause-active')
-                audio.pause()
-              } else {
-                this.playButton.forEach((playButton) => {
-                  playButton.classList.remove('pause-active')
-                  audio.pause()
-                })
+        this.playButton.forEach((item) => {
+          item.addEventListener('click', () => {
+            if (item.classList.contains('pause-active')) {
+              item.classList.remove('pause-active')
+              this.audioFile.pause()
+            } else {
+              this.playButton.forEach((playButton) => {
+                playButton.classList.remove('pause-active')
+                this.audioFile.pause()
+              })
 
-                item.classList.add('pause-active')
-                audio.play().then()
-              }
-            })
+              item.classList.add('pause-active')
+              this.audioFile.play().then()
+            }
           })
         })
       }
     }
 
     getTimeAudio (audioFile: HTMLAudioElement) {
+      console.log('audio time', audioFile)
+      const duration = audioFile.duration
+
+      const totalMinutes = Math.floor(duration / 60)
+      const totalSeconds = Math.floor(duration % 60)
+
+      this.totalTimeDisplay.forEach((item) => {
+        item.textContent = `${totalMinutes}:${totalSeconds < 10 ? '0' : ''}${totalSeconds}`
+      })
       audioFile?.addEventListener('timeupdate', () => {
         const currentTime = audioFile.currentTime
-        const duration = audioFile.duration
 
         const currentMinutes = Math.floor(currentTime / 60)
         const currentSeconds = Math.floor(currentTime % 60)
-        const totalMinutes = Math.floor(duration / 60)
-        const totalSeconds = Math.floor(duration % 60)
-
         this.currentTimeDisplay.forEach((item) => {
           item.textContent = `${currentMinutes}:${currentSeconds < 10 ? '0' : ''}${currentSeconds}`
         })
         this.totalTimeDisplay.forEach((item) => {
-          item.textContent = `${totalMinutes}:${totalSeconds < 10 ? '0' : ''}${totalSeconds}`
+          item.textContent = ''
         })
       })
     }
